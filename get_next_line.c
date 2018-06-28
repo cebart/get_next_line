66d11e1d1a51c196e0dcdcd4288276e94ff0da4e
cebart <cbartoli@protonmail.com>
2018-06-28 17:57:19 +0200

    en cours

--- get_next_line.c
+++ get_next_line.c
@@ -1,24 +1,51 @@
 #include "get_next_line.h"
 
-int get_next_line(const int fd, char **line)
+static char *fill_too_much_read (int j, char *buff)
 {
-    char        *buf;
-    int         ret;
-    int         i;
+    int     i;
+    char    *ret;
 
     i = 0;
+    *ret = ft_strnew(BUFF_SIZE);
+    while (buff[j])
+    {
+        ret[i] = buff[j];
+        i++;
+        j++;
+    }
+    return (ret);
+}
+
+int get_next_line(const int fd, char **line)
+{
+    char                *buf;
+    int                 ret;
+    int                 i;
+    int                 j;
+    static save_buff    sb; /* la list sera en static puis recup le bon maillon (too_much_read)
+                                en fonction du fd */
+    i = 0;
+    j = 0;
     ret = 1;
     buf = ft_strnew(BUFF_SIZE);
-    buf[0] = '\0';
+    buf[BUFF_SIZE - 1] = '\0';
+    sb.fd = fd;
+    sb.too_much_read = ft_strnew(BUFF_SIZE);
+    sb.too_much_read[0] = '\0';
     while (line[i][0])
         i++;
+    /* strcat de too_much_read sur line[i] /!\ '\n' dans too_much_read */
     while (ret != -1)
     {
-        ret = read (fd, buf, 1);
-        if (buf[0] == '\n')
+        ret = read (fd, buf, BUFF_SIZE);
+        while (buf[j] != '\n')
+        {
+            line[i] = ft_strcat(line[i], buf[j]);
+            j++;
+        }
+        sb.too_much_read = fill_too_much_read(j, buf);
+        if (buf[j] == '\n')
             ret = -1;
-        else
-            line[i] = ft_strcat(line[i], buf);
         if (ret == 0)
             return (0);
     }
--- get_next_line.h
+++ get_next_line.h
@@ -2,7 +2,14 @@
 # define GET_NEXT_LINE_H
 # include "libft/libft.h"
 # include "get_next_line.h"
-# define BUFF_SIZE 1
+# define BUFF_SIZE 10
+
+typedef struct save_buff
+{
+    int     fd;
+    char    *too_much_read;
+} save_buff;
+
 
 int get_next_line(const int fd, char **line);
 #endif
